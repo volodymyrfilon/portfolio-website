e9cc264676297e2d40aa01a7f7d40330499dd8c2
Volodymyr <volodymyrfilon1312@gmail.com>
2023-12-17 14:30:47 +0100

    feat: scroll hook

---
+++ hooks/useScrollProgress.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+
+const useScrollProgress = () => {
+	const [completion, setCompletion] = useState(0)
+
+	useEffect(() => {
+		const updateScrollCopmpletion = () => {
+			const currentProgress = window.scrollY
+			const scrollHeight = document.body.scrollHeight - window.innerHeight
+
+			if (scrollHeight) {
+				setCompletion(Number(currentProgress / scrollHeight).toFixed(2) * 100)
+			}
+		}
+
+		//event
+		window.addEventListener('scroll', updateScrollCopmpletion)
+		//clear event
+		return () => window.removeEventListener('scroll', updateScrollCopmpletion)
+	}, [])
+
+	console.log(completion)
+	return completion
+}
+
+export default useScrollProgress
